b3c6b94c9b3816d909ca023ae1ef1a789a57e540
evanator05 <watsonevan2005@gmail.com>
2023-08-18 23:16:20 -0600

    ai changes

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -8,9 +8,15 @@ var speed:float = 6
 var nextPoint:Vector3 = Vector3()
 
 @export var patrolPath:Array[Node3D]
-
+var currentPoint:int = 0 :
+	set(value):
+		if value > patrolPath.size()-1:
+			value = 0
+		if value < 0:
+			value = patrolPath.size()-1
+		currentPoint = value
+		
 var state = PATROL
-
 enum {PATROL, ALERT}
 
 func _ready():
@@ -19,12 +25,12 @@ func _ready():
 	getNextPoint()
 
 func _process(delta):
+	setTarget(patrolPath[currentPoint])
 	var moveDir:Vector3 = nextPoint-global_transform.origin
 	if moveDir.length() < 1: #if the distence to the current point is less than 1 get the next point
-		if navAgent.is_target_reached():
-			pass
+		if navAgent.distance_to_target() < 1:
+			currentPoint += 1
 		getNextPoint()
-	
 	moveDir = moveDir.normalized()
 	velocity = moveDir*speed
 	move_and_slide()
