64e696248d8d3690d5f9c0f8bc4e1431e418f0fc
evanator05 <watsonevan2005@gmail.com>
2023-08-19 13:27:51 -0600

    doors work

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -2,14 +2,23 @@ extends StaticBody3D
 
 @export var listenFor:ButtonManager
 
+var isOpen:bool = false
+
+@onready var startingPos:Vector3 = global_transform.origin
+@export var offset:Vector3
+
+
 func _ready():
 	listenFor.opened.connect(open)
 	listenFor.closed.connect(close)
 
+func _process(delta):
+	global_transform.origin = startingPos+offset
+
 func open():
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "global_transform:origin:y", global_transform.origin.y-4, 0.5)
+	tween.tween_property(self, "offset:y", -4, 0.5)
 	
 func close():
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "global_transform:origin:y", global_transform.origin.y+4, 0.5)
+	tween.tween_property(self, "offset:y", 0, 0.5)
