356242b9357a0a3e9eaba1a596f91d00d0b4ebd9
hughbagan <hbagan@ualberta.ca>
2023-08-20 12:54:13 -0600

    Title screen

---
+++ Scenes/Title.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_texture_button_pressed():
+	# Show exposition
+	$Intro.show()
+
+
+func _on_button_pressed():
+	var audioTween = get_tree().create_tween()
+	audioTween.tween_property($TitleMusic, "volume_db", -15.0, 5)
+	$BlackRect.show()
+	var toBlackTween = get_tree().create_tween()
+	toBlackTween.tween_property($BlackRect, "color:a", 1.0, 5)
+	await toBlackTween.finished
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
