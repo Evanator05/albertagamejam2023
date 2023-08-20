ddd80cf1991a8db902457c3622b8c47207ba7ffa
coltonix <7879984+coltonix@users.noreply.github.com>
2023-08-21 06:40:38 +0800

    Added Light Color/Flickering scripts

---
+++ Scripts/Light.gd
@@ -0,0 +1,11 @@
+class_name LightColor extends OmniLight3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	light_color = Color(0.941, 0.635, 0.102, 0.78)
