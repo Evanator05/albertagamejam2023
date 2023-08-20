66e4514eaec665a740603979b4741de228f08cc4
evanator05 <watsonevan2005@gmail.com>
2023-08-20 14:34:54 -0600

    ending???

--- Scripts/finalCutscene.gd
+++ Scripts/finalCutscene.gd
@@ -4,7 +4,8 @@ func _on_body_entered(body):
 	if body is Player:
 		body.state = body.frozen
 		var tween = get_tree().create_tween()
-		tween.tween_property($ColorRect, "color:a", 1, 5)
+		tween.tween_property($ColorRect, "color:a", 1, 3)
 		await tween.finished
 		#change to ending screen
+		get_tree().change_scene_to_file("res://Scenes/End.tscn")
 		
