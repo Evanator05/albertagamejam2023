119e17a2a60f82a765ec225eebc5e23403b3178e
hughbagan <hbagan@ualberta.ca>
2023-08-19 18:21:28 -0600

    Re-add elevator and merge

--- Objects/Elevator.gd
+++ Objects/Elevator.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 
 @onready var startingPos = self.global_transform.origin
+@onready var level = get_parent()
 @export var offset:Vector3 = Vector3()
 
 func _process(delta):
@@ -10,6 +11,7 @@ func _on_detect_area_body_entered(body):
 	if (body is Player):
 		# Go to next Level
 		body.state = body.frozen
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "offset:y", 10, 5)
-		#Levels.next_level();
+		#var tween = get_tree().create_tween()
+		#print(level)
+#		tween.tween_property(level, "glo", 10, 5)
+		Levels.nextLevel();
