9c067180300f8e94e399ea33905d9b997a4c02ee
coltonix <7879984+coltonix@users.noreply.github.com>
2023-08-21 07:35:03 +0800

    Applied Light to more lights

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -5,11 +5,12 @@ signal refreshLevel
 var mapSections
 var musicManager
 
+# Light Colors
 var colors = [
-	Color(1, 1, 1, 1),
-	Color(0.941, 0.635, 0.102, 0.78),
-	Color(0.941, 0.635, 0.102, 0.78),
-	Color(0.961, 0.773, 0.443, 0.78),
-	Color(0.941, 0.635, 0.102, 0.78),
-	Color(0.941, 0.635, 0.102, 0.78)
+	Color(0.878, 0.663, 0.086),
+	Color(0.988, 0.698, 0.184, 0.78),
+	Color(1, 0.741, 0.282, 0.78),
+	Color(1, 0.796, 0.439, 0.78),
+	Color(1, 0.914, 0.69),
+	Color(1, 0.965, 0.875)
 ]
