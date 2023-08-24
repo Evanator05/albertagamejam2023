f1fd672bd9e40ea45bba1af099fce7e10634c879
Aiden CC <a.collinscruickshank@gmail.com>
2023-08-23 19:38:10 -0600

    The title screen music now fades out again.

--- Scripts/Ui/Title.gd
+++ Scripts/Ui/Title.gd
@@ -10,5 +10,6 @@ func _on_button_pressed():
 	$BlackRect.show()
 	var toBlackTween = get_tree().create_tween()
 	toBlackTween.tween_property($BlackRect, "color:a", 1.0, 5)
+	$TitleEventEmitter2D.stop()
 	await toBlackTween.finished
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
