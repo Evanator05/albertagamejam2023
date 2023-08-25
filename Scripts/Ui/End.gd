f0fc7205b9a9c61b9f22f49e1172305ac99a3b49
Aiden CC <a.collinscruickshank@gmail.com>
2023-08-25 00:08:03 -0600

    End.gd now uses FMOD for audio

--- Scripts/Ui/End.gd
+++ Scripts/Ui/End.gd
@@ -5,13 +5,14 @@ func _ready():
 	toWhiteTween.tween_property($WhiteRect, "color:a", 0.0, 5)
 	await toWhiteTween.finished
 	$WhiteRect.hide()
+	
 
 func _on_button_pressed():
-	#var audioTween = get_tree().create_tween()
-	#audioTween.tween_property($EndMusic, "volume_db", -15.0, 5)
 	$BlackRect.show()
 	var toBlackTween = get_tree().create_tween()
 	toBlackTween.tween_property($BlackRect, "color:a", 1.0, 5)
+	await get_tree().create_timer(1.5).timeout#		(kind of) lines the music up with $Credits.show()
+	$CreditsEventEmitter2D.play()#					starts playing the music (with tween)
 	await toBlackTween.finished
 	$Button.hide()
 	$Credits.show()
@@ -19,4 +20,6 @@ func _on_button_pressed():
 
 
 func _on_title_button_pressed():
+	$CreditsEventEmitter2D.stop()#				starts fading out
+	await get_tree().create_timer(2).timeout#	AHDSR Release set to 2s in FMOD
 	get_tree().change_scene_to_file("res://Scenes/Ui/Title.tscn")
--- addons/FMOD/editor/fmod_guids.gd
+++ addons/FMOD/editor/fmod_guids.gd
@@ -11,23 +11,29 @@ class Events:
 	const MUSIC_ELEVATOR = "{a69e03e7-85c4-4f2b-9378-9bcf770f0283}"
 	const MUSIC_HOLDING_CELLS = "{182ce1a9-412c-44d2-b4bd-8b131cace5d6}"
 	const MUSIC_TITLE = "{9318af8b-cd8d-4c71-bed1-ab24aafd2753}"
-	const SFX_DOOR_CLOSE_SFX = "{b279548f-4346-4180-9223-cb2835ec8946}"
-	const SFX_DOOR_OPEN_SFX = "{b9f11043-4cf7-412c-ae59-548c73bcaa33}"
+	const SFX_DOOR_SFX = "{6aff6dd2-cf6a-405e-b93b-94d20ef9309f}"
 	const SFX_FOOTSTEP_SFX = "{21c7de01-0f18-4b7b-9e82-950d2b9ed233}"
-	const SFX_TRANQ_GUN_SFX_V2 = "{4e8899cd-1143-4f92-90d9-09eac37a559a}"
+	const SFX_TRANQ_GUN_V2_SFX = "{4e8899cd-1143-4f92-90d9-09eac37a559a}"
 
 class Busses:
 	const MASTER_BUS = "{08d2ba2c-c1a0-4d32-bb41-3f2fe0c282fc}"
 	const REVERB = "{f19ecf8c-8919-420c-8502-6f767364a6e6}"
 
 class Banks:
+	const MASTER = "{f93d27d8-c724-4ab3-8e95-d1b29ddf5674}"
+	const MUSIC = "{2c04129c-0190-4ea3-80dc-f2964deec5e5}"
 	const SCENES_CREDITS = "{a217c6b6-156e-4ee6-9b1d-9da58afd0fde}"
 	const SCENES_ELEVATOR = "{8a53844d-3e07-4dee-9e6d-077e9e69d0bc}"
 	const SCENES_HOLDING_CELLS = "{6c581e52-82c2-4bd6-954f-8473f244e1dc}"
+	const SCENES_LEVELS = "{fb26766d-22ea-400b-bdef-558c4351adf1}"
 	const SCENES_TITLE_SCREEN = "{0df20c02-bad7-4644-917e-b3936bf1fe32}"
-	const SFX_MASTER = "{f93d27d8-c724-4ab3-8e95-d1b29ddf5674}"
+	const SFX = "{1c4157e8-fcc8-4db7-aa3f-b60302a2f85f}"
 
 class Parameters:
-	const SPOTTEDBYGUARD = "{3f294c96-dd00-4a97-8b2a-6f33a5e115a4}"
-	const SPOTTEDBYGUARDSLIDER = "{162352db-79cd-4c98-b441-d2fc6813005d}"
+	const ACTIVEGUARDS = "{3f294c96-dd00-4a97-8b2a-6f33a5e115a4}"
+	const ACTIVEGUARDS_SLIDER = "{162352db-79cd-4c98-b441-d2fc6813005d}"
+	const AUDIOTWEEN = "{73c69632-e7d7-4b2f-b0d5-ecd3806b3f38}"
+	const DOORSTATUS = "{a22d3eb9-0a39-4c54-b927-2cb7670d8482}"
+	const HELICOPTOR_LOAD = "{fbe75e4e-1031-426f-9050-3ae3d9d5eed3}"
+	const HELICOPTOR_RPM = "{9731644c-c2fb-422c-8372-ebfb04ae8f2e}"
 
